5b16c28090d044fb8f17590aaec471dbe8e71074
sylvester <sylvestermbah90@gmail.com>
2026-07-22 16:39:05 -0700

    Create NotFound component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,23 @@
-
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section className='text-center py-20'>
+      <h1 className='text-5xl font-bold text-slate-800'>
+        404
+      </h1>
+
+      <p className='mt-4 text-slate-600'>
+        Sorry, the page you are looking for does not exist.
+      </p>
+
+      <Link
+        to={'/'}
+        className='inline-block mt-6 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700'
+      >
+        Go Home
+      </Link>
+    </section>
   )
 }
 
