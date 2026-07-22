aedf73b50b2a0dcb91c67eb2d95644123015c451
sylvester <sylvestermbah90@gmail.com>
2026-07-22 02:09:21 -0700

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t mt-auto">
+        <div className="container mx-auto px-4 py-4 text-center text-slate-700">
+            {new Date().getFullYear()} Weather App
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
