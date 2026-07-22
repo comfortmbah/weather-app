de6c31e5733b2cb8dbba737434b17bb8c1518690
sylvester <sylvestermbah90@gmail.com>
2026-07-22 01:56:37 -0700

    Configure the App.jsx for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+)
 
 const App = () => {
-  return (
-    <div className="text-center text-4xl">App</div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
