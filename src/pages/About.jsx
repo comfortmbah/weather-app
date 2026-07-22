9f91a8a0a4fa8b3fa4ed1d91b56eef2f04e4c21b
sylvester <sylvestermbah90@gmail.com>
2026-07-22 15:38:28 -0700

    Create About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,16 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">
+        About
+      </h1>
+
+      <p>
+        This weather App is built with React, React Router, TailwindCSS, and a weather API. 
+        It allows users to search for cities and display current weather information in a clean and responsive interface.
+      </p>
+    </section>
   )
 }
 
