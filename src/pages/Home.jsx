da279c89508f9e72d97012da4043c6e36a4b9bff
sylvester <sylvestermbah90@gmail.com>
2026-07-24 08:59:49 -0700

    Connect Homepage to weather API using useWaether hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import SearchForm from "../components/SearchForm"
 import PageTitle from "../components/PageTitle"
 import WeatherCard from "../components/WeatherCard"
+import Loader from '../components/Loader'
+import Error from '../components/Error'
+import EmptyState from '../components/EmptyState'
+import useWeather from "../hooks/useWeather"  
+import { useState } from "react"
 
 const Home = () => {
+  const [city, setCity] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const { weatherData, loading, error } = useWeather(searchCity);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!city.trim()) return;
+
+    setSearchCity(city);
+  }
+
   return (
     <>
       <PageTitle
@@ -10,16 +27,28 @@ const Home = () => {
         subtitle={"Search for any city around the world and view its current weather conditions."}
       />
 
-      <SearchForm />
-
-      <WeatherCard 
-        city={'Lagos'}
-        country={'Nigeia'}
-        temperature={29}
-        description={'Clear Sky'}
-        humidity={82}
-        windSpeed={15}
+      <SearchForm 
+        city={city}
+        onCityChange={(e) => setCity(e.target.value)}
+        onSearch={handleSubmit}
       />
+
+      {loading && <Loader />}
+
+      {error && <Error message={error} />}
+
+      {!loading && !error && !weatherData && <EmptyState />}
+
+      {weatherData && (
+        <WeatherCard 
+          city={weatherData.name}
+          country={weatherData.sys.country}
+          temperature={weatherData.main.temp}
+          description={weatherData.weatherData[0].description}
+          humidity={weatherData.main.humidity}
+          windSpeed={weatherData.wind.speed}
+        />
+      )}
     </>
   )
 }
