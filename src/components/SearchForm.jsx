5748ad2f173cc441e8a0937c15b35b1607433f0e
sylvester <sylvestermbah90@gmail.com>
2026-07-24 08:42:50 -0700

    Update the SearchForm component with some props.

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,13 +1,18 @@
+import PropTypes from 'prop-types'
 
-
-const SearchForm = () => {
+const SearchForm = ({ city, onCityChange, onSearch }) => {
   return (
-    <form className="max-w-2xl mx-auto flex flex-col sm:flex-row gap-4">
-        <input 
-          type="text"
-          placeholder="Enter city name..."
-          className="flex-1 border border-slate-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
-        />
+    <form
+      onSubmit={onSearch}
+      className="max-w-2xl mx-auto flex flex-col sm:flex-row gap-4"
+    >
+      <input 
+        type="text"
+        value={city}
+        placeholder="Enter city name..."
+        onChange={onCityChange}
+        className="flex-1 border border-slate-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
+      />
 
         <button
           className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
@@ -17,5 +22,11 @@ const SearchForm = () => {
     </form>
   )
 }
+
+SearchForm.propTypes = {
+  city: PropTypes.string.isRequired,
+  onCityChange: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
+};
 
 export default SearchForm
